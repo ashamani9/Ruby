501f696680925e8fd49fbe34f877b72655817596
ashamani9 <ashamani054@gmail.com>
2016-08-05 17:02:22 +0530

    swapping.rb

---
+++ swapping.rb
@@ -0,0 +1,15 @@
+#SWAPPING OF TWO VARIABLES USING THIRD VARIABLE
+
+print "Enter the first number:"
+a=gets.chomp.to_i
+print "Enter the second number:"
+b=gets.chomp.to_i
+c=0
+
+c=a
+a=b
+b=c
+
+
+puts "The numbers are: #{a}"
+puts "The number is :#{c}"
