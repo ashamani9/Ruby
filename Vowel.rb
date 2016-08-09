a4477fd3d9c8d42fc1303aff354464c851caf96a
Your Name <ashamani054@gmail.com>
2016-08-09 14:59:19 +0530

    Vowel.rb

---
+++ Vowel.rb
@@ -0,0 +1,10 @@
+# Checking the Alphabet is Vowel or not
+
+print "Please Enter the Character:"
+ch=gets.chomp.to_s
+
+if (ch=='a' || ch=='A' || ch=='e' || ch=='E' || ch=='i' || ch=='I' || ch=='o' || ch=='O' || ch=='u' || ch=='U')
+puts "This is a Vowel"
+else 
+puts "This is not a Vowel"
+end
